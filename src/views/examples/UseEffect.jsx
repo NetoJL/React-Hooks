4e476638d21b9718f455ef328d986fd9af319e77
José Neto <56655916+NetoJL@users.noreply.github.com>
2022-12-21 13:25:08 -0300

    useEffect finished

--- src/views/examples/UseEffect.jsx
+++ src/views/examples/UseEffect.jsx
@@ -1,15 +1,78 @@
-import React from 'react'
-import PageTitle from '../../components/layout/PageTitle'
+import React, { useEffect, useState } from "react";
+import PageTitle from "../../components/layout/PageTitle";
+import SectionTitle from "../../components/layout/SectionTitle";
 
-const UseEffect = (props) => {
-    return (
-        <div className="UseEffect">
-            <PageTitle
-                title="Hook UseEffect"
-                subtitle="Permite executar efeitos colaterais em componentes funcionais!"
-            />
-        </div>
-    )
+function calcFatorial(num) {
+	const n = parseInt(num);
+	if (n < 0) return -1;
+	if (n === 0) return 1;
+	return calcFatorial(n - 1) * n;
 }
 
-export default UseEffect
+function verifyNumber(num) {
+	const n = parseInt(num)
+	if (n < 0) return "Invalid number";
+	if (n % 2 === 0) return "Par"
+	if (n % 2 === 1) return "Impar"
+}
+
+const UseEffect = (props) => {
+	const [number, setNumber] = useState(1);
+	const [fatorial, setFatorial] = useState(1);
+
+	const [numberCheck, setNumberCheck] = useState(1);
+	const [check, setCheck] = useState("");
+
+	useEffect(
+		function () {
+			setFatorial(calcFatorial(number));
+		},
+		[number]
+	);
+	useEffect(
+		function () {
+			setCheck(verifyNumber(numberCheck));
+		},
+		[numberCheck]
+	);
+
+	return (
+		<div className="UseEffect">
+			<PageTitle
+				title="Hook UseEffect"
+				subtitle="Permite executar efeitos colaterais em componentes funcionais!"
+			/>
+
+			<SectionTitle title="Exercicio #01" />
+
+			<div className="center">
+				<div>
+					<span className="text">Fatorial: </span>
+					<span className="text red">{fatorial}</span>
+				</div>
+				<input
+					type="number"
+					className="input"
+					value={number}
+					onChange={(e) => setNumber(e.target.value)}
+				/>
+			</div>
+
+			<SectionTitle title="Exercicio #02" />
+			<div className="center">
+				<div>
+					<span className="text">Par ou Impar? </span>
+					<span className="text red">{check}</span>
+				</div>
+				<input
+					type="number"
+					className="input"
+					value={numberCheck}
+					onChange={(e) => setNumberCheck(e.target.value)}
+				/>
+			</div>
+		</div>
+	);
+};
+
+export default UseEffect;
