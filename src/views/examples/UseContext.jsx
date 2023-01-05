380b1dc3d63e62ca8c0d2035ff35f150a241f438
José Neto <56655916+NetoJL@users.noreply.github.com>
2023-01-05 15:37:06 -0300

    UseContext updated

--- src/views/examples/UseContext.jsx
+++ src/views/examples/UseContext.jsx
@@ -1,15 +1,39 @@
-import React from 'react'
-import PageTitle from '../../components/layout/PageTitle'
+import React, { useContext } from "react";
+import PageTitle from "../../components/layout/PageTitle";
+
+import DataContext from "../../data/DataContext";
 
 const UseContext = (props) => {
-    return (
-        <div className="UseContext">
-            <PageTitle
-                title="Hook UseContext"
-                subtitle="Aceita um objeto de contexto e retorna o valor atual do contexto!"
-            />
-        </div>
-    )
-}
+	const {state, setState} = useContext(DataContext);
 
-export default UseContext
+	function changeNumber(delta) {
+		setState({
+			...state,
+			number: state.number + delta,
+		});
+	}
+
+	return (
+		<div className="UseContext">
+			<PageTitle
+				title="Hook UseContext"
+				subtitle="Aceita um objeto de contexto e retorna o valor atual do contexto!"
+			/>
+
+			<div className="center">
+				<span className="text">{state.text}</span>
+				<span className="text">{state.number}</span>
+				<div>
+					<button className="btn" onClick={() => changeNumber(+1)}>
+						Add number
+					</button>
+					<button className="btn" onClick={() => changeNumber(-1)}>
+						Remove number
+					</button>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default UseContext;
