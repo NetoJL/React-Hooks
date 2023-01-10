776db066be5f48c2614298a0369a8e9ca7ba85ca
José Neto <56655916+NetoJL@users.noreply.github.com>
2023-01-10 16:48:28 -0300

    index.js (reducers) att

---
+++ src/store/reducers/index.js
@@ -0,0 +1,19 @@
+export function reducer(state, action) {
+	switch (action.type) {
+		case "numberAdd2":
+			return { ...state, number: state.number + 2 };
+		case "login":
+			return { ...state, user: { name: action.payload } };
+		case "number_multiply7":
+			return { ...state, number: state.number * 7 };
+		case "number_divide25":
+			return { ...state, number: parseInt(state.number / 25) };
+		case "number_userAdd":
+			return {
+				...state,
+				number: state.number + action.payload,
+			};
+		default:
+			return state;
+	}
+}
